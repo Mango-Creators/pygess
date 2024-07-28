9e83bddd751db58dfaef7f013748f497e6f1e7a4
Abhijay Bidari <craftyryte2011@gmail.com>
2024-07-28 09:32:18 +0530

    setup file

--- pygess/entity.py
+++ pygess/entity.py
@@ -1,5 +1,5 @@
 import pygame as pyg
-import pygess.data as data
+import data
 
 
 # Basic Entity
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Pygame Essentials'
+
+# Setting up
+setup(
+    name="pygess-py",
+    version=VERSION,
+    author="Abhijay 'CraftyRyte' Bidari",
+    author_email="<craftyryte2011@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pygame'],
+    keywords=['python', 'pygame', "essentials"],
+    classifiers=[
+        "Development Status :: 1 - Aplha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
