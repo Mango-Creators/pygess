3ad69cf40b27399a974aee0210cf5c2254f90392
Abhijay Bidari <craftyryte2011@gmail.com>
2024-07-30 14:38:58 +0530

    description changes

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
 
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 DESCRIPTION = 'Pygame Essentials'
+LONG_DESCRIPTION = "This is a library that makes developing in pygame simple for beginners. It has classes like BasicEntity, RectEntity, BasicMovingEntity etc. These 'templates' help beginners create their first games with ease. These templates are kept highly customizable and have a high scope for custom logic. This library currently (as of v0.1.1) contains only Entity classes and a master object list."
 
 # Setting up
 setup(
@@ -12,7 +13,7 @@ setup(
     author_email="craftyryte2011@gmail.com",
     license="The Unlicense",
     description=DESCRIPTION,
-    long_description="J",
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     packages=find_packages(),
     include_package_data=True,
