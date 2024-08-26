e4867935b22d6291cf0e99df5081f5995bd5c8a0
zeus <136583387+zeusssz@users.noreply.github.com>
2024-08-26 15:13:57 +0530

    Update entity.py

--- pygess/entity.py
+++ pygess/entity.py
@@ -4,22 +4,20 @@ from . import physics
 from . import colors
 
 class Entity(pyg.sprite.DirtySprite):
-    def __init__(self, position:tuple, dimensions:tuple, color=None, image_path=None) -> None:
-        pyg.sprite.Sprite.__init__(self)
-        
-        self.pos = pyg.math.Vector2(position[0], position[1])
+    def __init__(self, position: tuple, dimensions: tuple, color=None, image_path=None) -> None:
+        super().__init__()
+        self.pos = pyg.math.Vector2(position)
         self.dimensions = dimensions
         self.color = color
         
-        if isinstance(image_path, str) and image_path != None:
+        if isinstance(image_path, str) and image_path:
             self.image = pyg.image.load(image_path).convert()
             self.image = pyg.transform.scale(self.image, self.dimensions)
         else:
             self.image = pyg.Surface(self.dimensions)
             self.image.fill(self.color)
         
-        self.rect = self.image.get_frect()
-        self.rect.topleft = self.pos
+        self.rect = self.image.get_rect(topleft=self.pos)
         
         self.spr_group = pyg.sprite.GroupSingle()
         self.spr_group.add(self)
@@ -29,34 +27,11 @@ class Entity(pyg.sprite.DirtySprite):
         data.all_rects.append(self.rect)
     
     def update_rect(self):
-        index = data.all_rects.index(self.rect)
-        data.all_rects.remove(self.rect)
-        
-        
-        self.rect.x = self.pos.x
-        self.rect.y = self.pos.y
-        
-        self.rect.width = self.dimensions[0]
-        self.rect.height = self.dimensions[1]
-        
-    
-        data.all_rects.insert(index + 1, self.rect)
+        self.rect.topleft = self.pos
+        self.rect.size = self.dimensions
     
     def check_collisions(self):
-        for r in data.all_rects:
-            if r == self.rect:
-                continue
-            if not self.rect.colliderect(r):
-                continue
-            
-            else:
-               if r in self._colliding_objects:
-                self._colliding_objects.remove(r)
-                continue
-            
-            if r not in self._colliding_objects:
-                self._colliding_objects.append(r)
-                continue
+        self._colliding_objects = [r for r in data.all_rects if r != self.rect and self.rect.colliderect(r)]
     
     def is_colliding_with(self, rect):
         return rect in self._colliding_objects
@@ -65,23 +40,16 @@ class Entity(pyg.sprite.DirtySprite):
         return self._colliding_objects
 
     def update(self):
-        self.check_collisions()
         self.update_rect()
-        
+        self.check_collisions()
         if self in self.spr_group:
-            self.spr_group.remove(self)
             self.spr_group.update()
-            self.spr_group.add(self)
-        else:
-            self.spr_group.update()
-        
         self.spr_group.draw(pyg.display.get_surface())
 
 class MovingEntity(Entity):
     def __init__(self, position: tuple, dimensions: tuple, velocity: tuple, color=None, image_path=None) -> None:
-        Entity.__init__(self, position, dimensions, color, image_path)
-        self.velocity = pyg.math.Vector2(velocity[0], velocity[1])
+        super().__init__(position, dimensions, color, image_path)
+        self.velocity = pyg.math.Vector2(velocity)
 
     def move(self):
         self.pos += self.velocity * physics.Dt
-    
