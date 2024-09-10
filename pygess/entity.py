62bae3f6d8edd9296ea3f10140038511162d41dc
Abhijay Bidari <craftyryte2011@gmail.com>
2024-09-10 19:06:55 +0530

    Basic bug-less (or so we think) World system enabled

--- pygess/entity.py
+++ pygess/entity.py
@@ -1,17 +1,17 @@
 import pygame as pyg
 from . import data
 from . import physics
+from .physics import World
 
 class Entity(pyg.sprite.DirtySprite):
-    def __init__(self, position: tuple, dimensions: tuple, color=None, image_path=None) -> None:
+    def __init__(self, position: tuple, dimensions: tuple, world: tuple, color=None, image_path=None) -> None:
+    
         pyg.sprite.DirtySprite.__init__(self)
         self.pos = pyg.math.Vector2(position)
         self.dimensions = dimensions
         self.color = color
-        
-        self.worlds_to_exist = []
-        self.current_world: physics.World = None
-        self.previous_world = None
+
+        self.active_world = physics.get_active_world()
         
         if isinstance(image_path, str) and image_path:
             self.image = pyg.image.load(image_path).convert()
@@ -27,6 +27,10 @@ class Entity(pyg.sprite.DirtySprite):
 
         self._colliding_objects = []
         
+        for w in world:
+            if not isinstance(w, tuple):
+                w.add_gameobj((self,))
+            
         data.all_rects.append(self.rect)
     
     def update_rect(self):
@@ -48,52 +52,30 @@ class Entity(pyg.sprite.DirtySprite):
         return self._colliding_objects
 
     def update(self):
-        self.update_current_world()
-        
-        if self.current_world:
-            self.update_rect()
-            self.check_collisions()
-            
-            if self in self.spr_group:
-                self.spr_group.remove(self)
-                self.spr_group.update()
-                self.spr_group.add(self)
-            else:
-                self.spr_group.update()
-                
-            self.spr_group.draw(pyg.display.get_surface())
-            return
 
-        return
-    
-    def set_worlds_to_exist(self, *argv: physics.World):
-        for world in argv:
-            if world not in physics.get_all_worlds():
-                print("Unregistered World!")
-                continue
-            
-            self.worlds_to_exist.append(world)
-    
-    def update_current_world(self):
-        active_world = physics.get_active_world()
-            
-        if active_world in self.worlds_to_exist:
-            self.current_world = active_world
+        self.update_rect()
+        self.check_collisions()
+        
+        if self in self.spr_group:
+            self.spr_group.remove(self)
+            self.spr_group.update()
+            self.spr_group.add(self)
         else:
-            self.current_world = None
+            self.spr_group.update()
+            
+        self.spr_group.draw(pyg.display.get_surface())
+    
     
         
 class MovingEntity(Entity):
-    def __init__(self, position: tuple, dimensions: tuple, velocity: tuple, color=None, image_path=None) -> None:
-        Entity.__init__(self, position, dimensions, color, image_path)
+    def __init__(self, position: tuple, dimensions: tuple, velocity: tuple, world: tuple, color:tuple=None, image_path=None) -> None:
+        super().__init__(position, dimensions, world, color=color, image_path=image_path)
         self.velocity = pyg.math.Vector2(velocity)
         self.orignal_vel = pyg.math.Vector2(velocity)
         self.is_affected_by_gravity = False
 
     def move(self):
-        if self.current_world == None:
-            return
-        self.pos += self.velocity * self.current_world.dt
+        self.pos += self.velocity * self.active_world.dt
         
         if not self.is_affected_by_gravity:
             return
@@ -103,15 +85,3 @@ class MovingEntity(Entity):
     def set_gravitified(self, bool:bool):
         self.is_affected_by_gravity = bool
         
-    def update_current_world(self):
-        active_world = physics.get_active_world()
-        print(f"Active world: {active_world}, Current world: {self.current_world}")
-
-        if active_world in self.worlds_to_exist:
-            if self.current_world != None:
-                if active_world != self.current_world:
-                    print(f"World changed from {self.current_world} to {active_world}. Resetting velocity.")
-                    self.velocity = pyg.math.Vector2(0, 0)
-            self.current_world = active_world
-        else:
-            self.current_world = None
--- pygess/physics.py
+++ pygess/physics.py
@@ -4,16 +4,42 @@ import pygame as pyg
 
 worlds = []
 
+class GameObjects:
+    def __init__(self, entities:tuple) -> None:
+        self.entities = entities
+        
+    def update(self):
+        from . import entity as ent
+        self.entities = tuple(set(self.entities))
+        for entity in self.entities:
+            if isinstance(entity, (ent.Entity)):
+                entity.update()
+            
+    def append(self, entity: tuple):
+        for e in entity:
+            if e != None:
+                a = list(self.entities)
+                a.append(e)
+                self.entities = tuple(a)
+
 class World:
-    def __init__(self, grav: tuple) -> None:
+    def __init__(self, grav: tuple, game_objects: tuple = ()) -> None:
         self.gravity = pyg.math.Vector2(grav)
         self.dt = 0
         self.prev_time = time.time()
         self.is_active = False
         
+        self.objects = GameObjects(game_objects)
+        
         worlds.append(self)
         
+    def update(self):
+        self.objects.update()
     
+    def add_gameobj(self, gameobj: tuple):
+        for obj in gameobj:
+            self.objects.append((obj,))
+        
     def update_delta_time(self):
 
         if self.is_active: 
@@ -23,7 +49,7 @@ class World:
         self.dt = get_active_world().dt
         self.dt = min(self.dt, 0.1)
 
-def get_active_world():
+def get_active_world() -> World:
     for world in worlds:
         if world.is_active:
             return world
